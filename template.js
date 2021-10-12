e9532f2732be56d5ac2feea1fe67dabd93ab7343
cerealenjoyer <59370143+cerealenjoyer@users.noreply.github.com>
2021-10-12 17:44:08 +0100

    Create template.js

---
+++ template.js
@@ -0,0 +1,10 @@
+// Here's where the head is gonna go for every page (to keep it optimised and accessible)
+// It'll look something like 
+/* function createPage(html, title) {
+   `< head stuff >
+   <title>${title}</title>
+   <body>
+   ${html}
+   </body>`
+   }
+   */
