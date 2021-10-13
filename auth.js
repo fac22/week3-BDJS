427bd2660067aec913b06e3be12af63f896bd9e6
jihye <jiji.pak41@gmail.com>
2021-10-13 15:40:10 +0100

    create functions - createUser, saveUserSession, verifyUser
    Co-authored-by: 0bubbles0 <77367593+0bubbles0@users.noreply.github.com

--- auth.js
+++ auth.js
@@ -1,6 +1,35 @@
-// Imports
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const model = require('./database/model');
 
-// Export to /routes
-module.exports = {};
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  maxAge: 600000,
+  sameSite: 'strict',
+  signed: true,
+};
+
+function createUser(email, password, name) {
+  return bcrypt
+    .hash(password, 10)
+    .then((hash) => model.createUser(email, hash, name));
+}
+
+function saveUserSession(user) {
+  const sid = crypto.randomBytes(18).toString('base64');
+  return model.createSession(sid, { user });
+}
+
+function verifyUser(email, password) {
+  return model.getUser(email).then((user) => {
+    return bcrypt.compare(password, user.password).then((match) => {
+      if (!match) {
+        throw new Error('Password mismatch');
+      } else {
+        return user; // ???
+      }
+    });
+  });
+}
+
+module.exports = { createUser, saveUserSession, verifyUser, COOKIE_OPTIONS };
