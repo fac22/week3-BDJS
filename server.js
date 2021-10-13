4d1d14f66940b4c3576170bc8568ff503e3b7e61
jihye <jiji.pak41@gmail.com>
2021-10-13 12:18:09 +0100

    userver.js set cookie parser
    Co-authored-by: danilo-cupido <81513148+danilo-cupido@users.noreply.github.com>
    Co-authored-by: cerealenjoyer <59370143+cerealenjoyer@users.noreply.github.com>
    Co-authored-by: 0bubbles0 <77367593+0bubbles0@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-// Nothing here...yet?
+'use strict';
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const home = require('./routes/home.js');
+const login = require('./routes/login.js');
+const logout = require('./routes/logout.js');
+const signup = require('./routes/signup.js');
+
+const server = express();
+
+server.use(express.urlencoded({ extended: false }));
+
+// COOKIE_SECRET lives in .env to stop it ending up on GitHub
+// it is used to sign cookies so we can trust them
+server.use(cookieParser(process.env.COOKIE_SECRET));
+
+server.get('/', home.get);
+
+server.get('/sign-up', signup.get);
+server.post('/sign-up', signup.post);
+
+server.get('/log-in', login.get);
+server.post('/log-in', login.post);
+
+server.post('/log-out', logout.post);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
