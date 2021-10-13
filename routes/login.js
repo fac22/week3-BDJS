57f38d920f70bee8dc0fe107ab3d7a23de99543d
jihye <jiji.pak41@gmail.com>
2021-10-13 15:02:57 +0100

    create a form for login
    Co-authored-by: 0bubbles0 <77367593+0bubbles0@users.noreply.github.com

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,20 @@
 // Imports
 const auth = require('../auth.js');
-const template = require('../template');
+const { buildPage } = require('../template.js');
 
 function get(request, response) {
-  //
+  const title = `coffe-login`;
+  const content = `
+    <h1>Log in</h1>
+    <form action="log-in" method="POST">
+      <label for="email">Email</label>
+      <input type="email" id="email" name="email">
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password">
+      <button>Log in</button>
+    </form>
+  `;
+  response.send(buildPage(title, content));
 }
 
 function post(request, response) {
