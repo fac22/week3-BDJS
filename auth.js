fe60fa09b29dcd922b4b4ed5c165e8475ca1dbae
danilo-cupido <danilocupido@gmail.com>
2021-10-13 15:42:45 +0100

    create authentication functions
    Co-authored-by: cerealenjoyer <59370143+cerealenjoyer@users.noreply.github.com>
    Co-authored-by: 0bubbles0 <77367593+0bubbles0@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>

--- auth.js
+++ auth.js
@@ -1,6 +1,34 @@
 // Imports
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const model = require('./database/model');
 
+async function verifyUser(email, password) {
+  const user = await model.getUser(email);
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw new Error('Password mismatch');
+  } else {
+    delete user.password;
+    return user;
+  }
+}
+
+async function createUser(name, email, password, drinkorder) {
+  const hash = await bcrypt.hash(password, 10);
+  return await model.createUser(name, email, hash, drinkorder);
+}
+
+function saveUserSession(user) {
+  const sid = crypto.randomBytes(18).toString('base64');
+  return model.createSession(sid, { user });
+}
+
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  maxAge: 600000,
+  sameSite: 'strict',
+  signed: true,
+};
 // Export to /routes
-module.exports = {};
+module.exports = { verifyUser, createUser, saveUserSession, COOKIE_OPTIONS };
