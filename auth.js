40f84514aadb9b84ffab01d3ab092fa2907d2e49
jihye <jiji.pak41@gmail.com>
2021-10-13 12:16:49 +0100

    update model.js and auth.js - import and export every routes
    Co-authored-by: danilo-cupido <81513148+danilo-cupido@users.noreply.github.com>
    Co-authored-by: cerealenjoyer <59370143+cerealenjoyer@users.noreply.github.com>
    Co-authored-by: 0bubbles0 <77367593+0bubbles0@users.noreply.github.com>

---
+++ auth.js
@@ -0,0 +1,6 @@
+// Imports
+const bcrypt = require('bcryptjs');
+const model = require('./database/model');
+
+// Export to /routes
+module.exports = {};
---
+++ database/model.js
@@ -0,0 +1,5 @@
+// Import database reference
+const db = require('./connection.js');
+
+// Export to auth.js (?and /routes)
+module.exports = {};
