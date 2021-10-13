d2f13ea43cb5497ff1a84cde95211e6e479cf378
jihye <jiji.pak41@gmail.com>
2021-10-13 14:58:56 +0100

    create a form for sign up
    Co-authored-by: 0bubbles0 <77367593+0bubbles0@users.noreply.github.com

--- routes/signup.js
+++ routes/signup.js
@@ -1,8 +1,22 @@
 const auth = require('../auth.js');
-const template = require('../template');
+const { buildPage } = require('../template.js');
 
 function get(request, response) {
   //
+  const title = `coffee-signup`;
+  const content = `
+  <h1>Create an account</h1>
+  <form action="sign-up" method="POST">
+    <label for="name">Name</label>
+    <input type="text" id="name" name="name">
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email">
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+    <button>Sign up</button>
+  </form>
+  `;
+  response.send(buildPage(title, content));
 }
 
 function post(request, response) {
