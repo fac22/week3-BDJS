fab0b2e19c33fd6a1148cbd8cded552c8f6b9f0e
danilo-cupido <danilocupido@gmail.com>
2021-10-14 11:00:57 +0100

    reverse async funtions to promises
    Co-authored-by: cerealenjoyer <59370143+cerealenjoyer@users.noreply.github.com>
    Co-authored-by: 0bubbles0 <77367593+0bubbles0@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>

--- auth.js
+++ auth.js
@@ -5,29 +5,52 @@ const model = require('./database/model');
 const COOKIE_OPTIONS = {
   httpOnly: true,
   maxAge: 600000,
-  sameSite: 'strict',
+  sameSite: 'lax',
   signed: true,
 };
 
-async function verifyUser(email, password) {
-  const user = await model.getUser(email);
-  const match = await bcrypt.compare(password, user.password);
-  if (!match) {
-    throw new Error('Password mismatch');
-  } else {
-    delete user.password;
-    return user;
-  }
+// async function verifyUser(email, password) {
+//   const user = await model.getUser(email);
+//   const match = await bcrypt.compare(password, user.password);
+//   if (!match) {
+//     throw new Error('Password mismatch');
+//   } else {
+//     delete user.password;
+//     return user;
+//   }
+// }
+
+function verifyUser(email, password) {
+  return model.getUser(email).then((user) => {
+    return bcrypt.compare(password, user.password).then((match) => {
+      if (!match) {
+        throw new Error("password doesn't match!");
+      } else {
+        delete user.password;
+        return user;
+      }
+    });
+  });
 }
 
-async function saveUserSession(user) {
-  const sid = await crypto.randomBytes(18).toString('base64');
-  return await model.createSession(sid, { user });
-}
+// async function saveUserSession(user) {
+//   const sid = await crypto.randomBytes(18).toString('base64');
+//   return await model.createSession(sid, { user });
+// }
 
-async function createUser(name, email, password, drinkorder) {
-  const hash = await bcrypt.hash(password, 10);
-  return await model.createUser(name, email, hash, drinkorder);
+function saveUserSession(user) {
+  const sid = crypto.randomBytes(18).toString('base64');
+  return model.createSession(sid, { user });
+}
+// async function createUser(name, email, password, drinkorder) {
+//   const hash = await bcrypt.hash(password, 10);
+//   return await model.createUser(name, email, hash, drinkorder);
+// }
+
+function createUser(name, email, password, drinkorder) {
+  return bcrypt
+    .hash(password, 10)
+    .then((hash) => model.createUser(name, email, hash, drinkorder));
 }
 
 module.exports = { createUser, saveUserSession, verifyUser, COOKIE_OPTIONS };
