ccef6736cf307226976659836617797e670abcba
danilo-cupido <danilocupido@gmail.com>
2021-10-14 11:04:43 +0100

    reverse async funtions to promises
    Co-authored-by: cerealenjoyer <59370143+cerealenjoyer@users.noreply.github.com>
    Co-authored-by: 0bubbles0 <77367593+0bubbles0@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -1,39 +1,42 @@
 const db = require('./connection.js');
 
-async function createUser(name, email, hash, drinkorder) {
+function createUser(name, email, hash, drinkorder) {
   const INSERT_USER = `
-  INSERT INTO users (name, email, hash, drinkorder) VALUES ($1, $2, $3, $4)
+  INSERT INTO users (name, email, password, drinkorder) VALUES ($1, $2, $3, $4)
   RETURNING id, email, name
   `;
-  const query = await db.query(INSERT_USER, [name, email, hash, drinkorder]);
-  return await query.rows[0].sid;
+  return db
+    .query(INSERT_USER, [name, email, hash, drinkorder])
+    .then((result) => result.rows[0]);
 }
 
-async function getUser(email) {
+// Got this from a workshop solution:
+function getSession(sid) {
+  const SELECT_SESSION = 'SELECT data FROM sessions WHERE sid=$1';
+  return db.query(SELECT_SESSION, [sid]).then((result) => {
+    const singleResult = result.rows[0];
+    return singleResult && singleResult.data;
+  });
+}
+
+function getUser(email) {
   const SELECT_USER = `
     SELECT id, email, password, name FROM users WHERE email=$1
   `;
-  const user = await db.query(SELECT_USER, [email]);
-  return user.rows[0];
+  return db.query(SELECT_USER, [email]).then((result) => result.rows[0]);
 }
 
-async function getSession(sid) {
-  const SELECT_SESSION = 'SELECT data FROM sessions WHERE sid=$1';
-  const session = await db.query(SELECT_SESSION, [sid]);
-  const singleResult = session.rows[0];
-  return singleResult && singleResult.data;
-}
-
-async function createSession(sid, json) {
+function createSession(sid, json) {
   const INSERT_SESSION = ` INSERT INTO sessions (sid, data) VALUES ($1, $2)
     RETURNING sid`;
-  const query = await db.query(INSERT_SESSION, [sid, json]);
-  return query.rows[0].sid;
+  return db
+    .query(INSERT_SESSION, [sid, json])
+    .then((response) => response.rows[0].sid);
 }
 
-async function deleteSession(sid) {
+function deleteSession(sid) {
   const DELETE_SESSION = 'DELETE FROM sessions WHERE sid=$1';
-  return await db.query(DELETE_SESSION, [sid]);
+  return db.query(DELETE_SESSION, [sid]);
 }
 
 module.exports = {
