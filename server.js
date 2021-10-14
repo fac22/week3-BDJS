415f4e36350c0d20f30eea4026bfd409f91a89bf
jihye <jiji.pak41@gmail.com>
2021-10-14 17:50:24 +0100

    revise port

--- server.js
+++ server.js
@@ -34,8 +34,7 @@ server.post('/edituser', edit.post);
 server.post('/logout', logout.post);
 server.post('/deletecoffee', remove.post);
 
-const PORT = 3000;
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
